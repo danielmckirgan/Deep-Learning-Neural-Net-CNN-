5bc02b697e190b7f29fc002b4dd0d7dd990710c8
Daniel McKirgan <90268829+danielmckirgan@users.noreply.github.com>
2023-02-16 16:47:25 -0500

    Update plant_detection_model.py

--- plant_detection_model.py
+++ plant_detection_model.py
@@ -6,90 +6,66 @@
 # feed the data through the model and give the final accuracy with a graph of the accuracy across the epochs and the
 # cross-entropy loss.
 
-# imports
-# Packages used to create model
 import keras
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 from keras_preprocessing import image
-# Packages to set arrays and split data
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-# Plotting packages
-import matplotlib.pyplot as pyplot
-import matplotlib.pyplot as plt
-# Misc
-import sys
 from tqdm import tqdm
+import matplotlib.pyplot as pyplot
+import sys
 
-# read in csv file, in this case I had a tsv so the sep='\t' will tell the function the file is tab separated.
-# Line 32 should be the column name that you put the concatenated Sample Number and Timestamp (if you did not read the README the instructions
-# for that are in there). We want the column to be read in as a string because if not the program will think it is a float and will leave off
-# any trailing 0's. These 0's are important later when the program is trying to iterate through yout metadata file and match that to the pixel data.
-
-train = pd.read_csv('Filepath to metadata/categorical data: Project/metadata/...', sep='\t',
-                    dtype={'Your Concatenated Sample Number': str})
-
-# Print train to ensure the data is correctly formatted
+train = pd.read_csv(
+    r'C:\Your Project Path\ ... \sampleMetadata_.txt', sep='\t', dtype={'True_Samp_Num': str})
 print(train)
 print(train.columns)
-
 train_image = []
-
-# Iterate through image file to extract the image data
 for i in tqdm(range(train.shape[0])):
     img = image.load_img(
-        r'Filepath to image folder: Project/Images/...' + str(train['(Same from line 32'][i]) +
-        '.png', target_size=(100, 100, 3))  # target size smaller than 250x250 for quicker processing, but this will
-    # change the size they are read in at
+        r'C:\Your Project path\... \overview.' + str(train['True_Samp_Num'][i]) +
+        '.png', target_size=(100, 100, 3)) #resized to decrease runtime
     img = image.img_to_array(img)
     img = img / 255
     train_image.append(img)
 
-# Assign values to images and split images into test and train groups
 X = np.array(train_image)
+y = np.array(train.drop({'True_Samp_Num', 'Sample Number', 'Timestamp', 'AdjSampNum'}, axis=1))
 
-# Here we want to get rid of any category in the metadata file that will not be a categorical output, for example
-# "Sample Number" is not a value we want output from the model, so we will drop it. This step is much easier if you have cleaned up your excel data
-y = np.array(train.drop({'Any categorical value in metadata file that is not an output'}, axis=1))
-
-# Splitting the dataset into training and testing, this will be done randomly every time the program is run
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
 
 
-# Define Model, this is the model used for the initial dataset, this will require some tweaking depending on your
-# dataset
+# Define Model
 def model_definition():
     model = Sequential()
-    # 1st Group w/ Max Pooling and Dropout at 20%
+    # 1st Group w/ Max Pooling and Batch Normalization
     model.add(keras.Input(shape=(100, 100, 3)))
     model.add(Conv2D(32, activation='relu', kernel_size=(5, 5)))
     model.add(MaxPooling2D((2, 2)))
     model.add(Dropout(0.2))
 
-    # 2nd Group w/ Max Pooling and Dropout at 30%
+    # 2nd Group w/ Max Pooling, Batch Normalization, and Dropout at 20%
     model.add(Conv2D(64, activation='relu', kernel_size=(5, 5)))
     model.add(MaxPooling2D((2, 2)))
     model.add(Dropout(0.3))
 
-    # 3rd Group w/ Max Pooling and Dropout at 40%
+    # 3rd Group w/ Max Pooling, Batch Normalization, and Dropout at 30%
     model.add(Conv2D(128, activation='relu', kernel_size=(5, 5)))
     model.add(MaxPooling2D((2, 2)))
     model.add(Dropout(0.4))
 
-    # Transform model into 1D array and pass into Dense
+    # Transform model into 1D array and pass into Dense functions
     model.add(Flatten())
-
     model.add(Dense(6, activation='sigmoid'))
     model.summary()
-    # We use the Adam optimizer and binary_crossentropy as the loss function to categorize the pixels
+    # Adam optimizer, binary_crossentropy for column selection(each value in each column contains either a 1 or 0),
+    # accuracy as metric to assess how the model is running
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
 
-# Plot the Cross Entropy Loss and Classification Accuracy
-# Creates 2 graphs, one plotting the accuracy and one plotting the loss on the Validation sets
 def summarize_diagnostics(history):
     # plot loss
     pyplot.subplot(211)
@@ -107,10 +83,7 @@ def summarize_diagnostics(history):
     pyplot.savefig(filename + '_plot.png')
     pyplot.close()
 
-# This is the method that tells our model how to run, 35 epochs means the data will run through the model 35 times,
-# each batch of pixels used is 64, the validation is our test sets we made on Line 50, verbose will show us each
-# individual step, and steps_per_epoch is how many times the weights and biases are getting updated during the epoch.
-# It is common practice for the steps_per_epoch to be (size of array/ batch_size), in this case that was 88.
+
 def run_test_harness():
     model = model_definition()
     steps = int(X_train.shape[0] / 64)
@@ -119,8 +92,7 @@ def run_test_harness():
     _, acc = model.evaluate(X_test, y_test, verbose=0)
     print('>%.3f' % (acc * 100))
     summarize_diagnostics(history)
+    model.save('base_model.h5')
 
 
-# run the model
-# This will run the previous function and train the dataset
 run_test_harness()
